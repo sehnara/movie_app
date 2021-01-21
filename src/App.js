1037aee1dde468432bc1c8dc0b89039eb30b4a94
= <=>
2021-01-21 22:33:14 +0900

    Revert "movie app"
    This reverts commit 26a3fda017311662f52efd8649ba42e64ba6c85d.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import axios from "axios";
 
 class App extends React.Component{ 
   state = {
     isLoading : true    
   };
   componentDidMount(){
-    axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    setTimeout(()=>{
+      this.setState({isLoading: false})
+    },6000);
   }
   render(){
     const { isLoading } = this.state;
